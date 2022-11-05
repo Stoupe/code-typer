7548bb846406a91547ecdfe56ad680c626d7f0dd
Henry Stoupe <h.stoupe@gmail.com>
2022-11-05 23:17:50 +1300

    Update inference helpers for trpc 10-rc.1

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,6 @@
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
-import { type GetInferenceHelpers } from "@trpc/server";
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 
 import { type AppRouter } from "../server/trpc/router/_app";
@@ -32,6 +32,12 @@ export const trpc = createTRPCNext<AppRouter>({
 
 /**
  * Inference helpers
- * @example type HelloOutput = RouterTypes['example']['hello']['output']
+ * @example type HelloOutput = RouterOutput['example']['hello']
  **/
-export type RouterTypes = GetInferenceHelpers<AppRouter>;
+export type RouterInput = inferRouterInputs<AppRouter>;
+
+/**
+ * Inference helpers
+ * @example type HelloOutput = RouterOutput['example']['hello']
+ **/
+export type RouterOutput = inferRouterOutputs<AppRouter>;
